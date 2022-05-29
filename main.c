701a8a5cac1887d5fc1e2d22e6b245fca94ebcf9
Azizbourmel <97178648+Azizbourmel@users.noreply.github.com>
2022-05-29 20:23:53 +0100

    Add files via upload

--- main.c
+++ main.c
@@ -3,27 +3,15 @@
 
 int main()
 {
-    const int taille_string=100;
-    char date[taille_string];
-    char nom[taille_string];
-    char prenom[taille_string];
-    char couleur[taille_string];
-    //saiser utilisateur
-    printf("saiser les informations suivantes\n");
+  int tab [2][3]={{1,2,3},{4,5,6} };
+  for(int ligne=0;ligne<2;ligne++)
+  {
+      int* p_ligne = *(tab+ligne);
+      for(int colone=0;colone<3;colone++)
+      {
+          printf("tab[%d][%d]=%d\n",ligne,colone,*(p_ligne+colone));
+      }
+  }
 
-    printf("\t la date de naissance jj/mm/aaaa \n");
-    scanf("%s", date);
-
-    printf("\t votre nom\n");
-    scanf("%s", nom);
-
-    printf("votre prenom\n");
-    scanf("%s", prenom);
-
-    printf("\t la coleur des yeux\n");
-    scanf("%s", couleur);
-
-    //affichage des information
-    printf("vous appelez %s %s vous etes nes le %s vos yeux ont %s \n",nom, prenom, date, couleur);
-     return 0;
+    return 0;
 }
